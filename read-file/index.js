89e5d8eef39f8dd59c71a7f5c9f17bf5e9562d63
obrunofontana <obrunofontana@gmail.com>
2022-06-30 20:25:59 -0300

    feat: implementa adicionar e remover em um arquivo json

--- read-file/index.js
+++ read-file/index.js
@@ -1,15 +1,42 @@
 const http = require('http');
 const URL = require('url');
-const users = require('./users.json');
+const fs = require('fs');
+const path = require('path');
+let users = require('./users.json');
 
-http.createServer((request, response) => {
-  // Responde pro cliente;
-  const { name, lastName, age } = URL.parse(request.url, true).query;
-  if (name || lastName || age) {
-    console.log('query',  name, lastName, age);
+const getUsers = (request, response) => {
+  const { name, lastName, age, remove } = URL.parse(request.url, true).query;
+  
+  if (name) {    
+    if (remove) {
+      users = users.filter(user => String(user.name) !== String(name));
+    } else {
+      const user = {
+        name, lastName, age
+      }
+      users.push(user);
+    }
+      
+    fs.writeFile(
+      path.join(__dirname, 'users.json'),
+      JSON.stringify(users, null, 2),
+      (error) => {
+        if (error) return;
+
+        console.log('Salvou o registro com sucesso');
+      }
+    );
   }
 
   response.end(JSON.stringify(users));
-}).listen(3001, () => {
-  console.log('API is running on port 3001');
-})
+}
+
+const server = http.createServer(getUsers);
+
+server.listen(3001, () => {
+  console.log('API listening on http://localhost:3001');
+});
+
+
+
+
