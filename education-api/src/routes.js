8ef8b085c1f1da7d72086e73546c6d338434d63c
obrunofontana <obrunofontana@gmail.com>
2022-07-18 21:06:02 -0300

    feat: 18-06: implementa metodo create instructor

--- education-api/src/routes.js
+++ education-api/src/routes.js
@@ -4,10 +4,13 @@ const routes = express.Router();
 
 const IndexController = require('./controllers/IndexController'); 
 const CoursesController = require('./controllers/CoursesController');
+const InstructorsController = require('./controllers/InstructorsController');
 
 routes.get('/', IndexController.index);
 
 routes.post('/courses', CoursesController.create);
+
+routes.post('/instructors', InstructorsController.create);
 
 module.exports = routes;
 
