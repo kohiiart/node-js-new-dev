95e24ba0d013d4db1e64af6b5a5a7a27f062f695
Kohii <104533272+kohiiart@users.noreply.github.com>
2022-07-21 20:40:50 -0300

    education-api
    Enviando para produção

--- api-rest/src/databases/knex.js
+++
@@ -1,8 +0,0 @@
-const environment = 
-  process.env.ENVIRONMENT || 'development';
-
-const config = require('../../knexfile')[environment];
-
-const knex = require('knex')(config);
-
-module.exports = knex;
